fae6d6cfd96a08d2ddc2055f8569d128aea91cdc
borisbrunop <borisbruno88@gmail.com>
2020-11-24 12:03:16 -0400

    menu bar a mitad de camino

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,194 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import BusinessIcon from "@material-ui/icons/Business";
+import TrendingUpIcon from "@material-ui/icons/TrendingUp";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import ImportContactsIcon from "@material-ui/icons/ImportContacts";
+import Avatar from "@material-ui/core/Avatar";
+import Logo from "../../img/logo azul.png";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import PropTypes from "prop-types";
 
-export const Navbar = () => {
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		display: "flex"
+	},
+	appBar: {
+		backgroundColor: "rgba(250, 250, 250, 0.9)",
+		transition: theme.transitions.create(["margin", "width"], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		})
+	},
+	appBarShift: {
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create(["margin", "width"], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		}),
+		marginRight: drawerWidth
+	},
+	title: {
+		flexGrow: 1
+	},
+	hide: {
+		display: "none"
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0
+	},
+	drawerPaper: {
+		width: drawerWidth
+	},
+	drawerHeader: {
+		display: "flex",
+		alignItems: "center",
+		padding: theme.spacing(0, 1),
+		// necessary for content to be below app bar
+		...theme.mixins.toolbar,
+		justifyContent: "flex-start"
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(3),
+		transition: theme.transitions.create("margin", {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		}),
+		marginRight: -drawerWidth
+	},
+	contentShift: {
+		transition: theme.transitions.create("margin", {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		}),
+		marginRight: 0
+	}
+}));
+function ElevationScroll(props) {
+	const { children, window } = props;
+	// Note that you normally won't need to set the window ref as useScrollTrigger
+	// will default to window.
+	// This is only being set here because the demo is in an iframe.
+	const trigger = useScrollTrigger({
+		disableHysteresis: true,
+		threshold: 0,
+		target: window ? window() : undefined
+	});
+
+	return React.cloneElement(children, {
+		elevation: trigger ? 4 : 0
+	});
+}
+
+ElevationScroll.propTypes = {
+	children: PropTypes.element.isRequired,
+	/**
+	 * Injected by the documentation to work in an iframe.
+	 * You won't need it on your project.
+	 */
+	window: PropTypes.func
+};
+
+export const Navbar = props => {
+	const classes = useStyles();
+	const theme = useTheme();
+	const [open, setOpen] = React.useState(false);
+
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
+
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<div className={classes.root}>
+			<CssBaseline />
+			<ElevationScroll {...props}>
+				<AppBar
+					position="fixed"
+					className={clsx(classes.appBar, {
+						[classes.appBarShift]: open
+					})}>
+					<Toolbar>
+						<div className={classes.title}>
+							<Avatar src={Logo} noWrap />
+						</div>
+						<IconButton
+							aria-label="open drawer"
+							edge="end"
+							onClick={handleDrawerOpen}
+							className={clsx(open && classes.hide)}>
+							<MenuIcon />
+						</IconButton>
+					</Toolbar>
+				</AppBar>
+			</ElevationScroll>
+			<Drawer
+				className={classes.drawer}
+				variant="persistent"
+				anchor="right"
+				open={open}
+				classes={{
+					paper: classes.drawerPaper
+				}}>
+				<div className={classes.drawerHeader}>
+					<IconButton onClick={handleDrawerClose}>
+						{theme.direction === "rtl" ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+					</IconButton>
+				</div>
+				<Divider />
+				<List>
+					<ListItem button>
+						<ListItemIcon>
+							<BusinessIcon />
+						</ListItemIcon>
+						<ListItemText>Cormineco</ListItemText>
+					</ListItem>
+					<ListItem button>
+						<ListItemIcon>
+							<VerifiedUserIcon />
+						</ListItemIcon>
+						<ListItemText>Compromiso</ListItemText>
+					</ListItem>
+					<ListItem button>
+						<ListItemIcon>
+							<TrendingUpIcon />
+						</ListItemIcon>
+						<ListItemText>Alcance</ListItemText>
+					</ListItem>
+					<ListItem button>
+						<ListItemIcon>
+							<ImportContactsIcon />
+						</ListItemIcon>
+						<ListItemText>Contacto</ListItemText>
+					</ListItem>
+				</List>
+			</Drawer>
+		</div>
 	);
 };
+
+// navBar: {
+// 	backgroundColor: "rgba(250, 250, 250, 0.9)"
+// }
